eb750478392253eab1461ae1d9a234b9574d14ea
tikutikurin <sdb01074@st.osakafu-u.ac.jp>
2022-11-14 00:52:45 +0900

    Please enter the commit message for your changes. Lines startingi with '#' will be ignored, and an empty message aborts the commit.

--- CA_train.py
+++ CA_train.py
@@ -2,5 +2,18 @@ import numpy as np
 from scipy.sparse import dok_matrix
 from sklearn.base import BaseEstimator, ClusterMixin
 
+class CA(BaseEstimator, ClusterMixin):
+
+    NOISE_LABEL = -1
+
+    def __init__(self, Lambda = 50, minCIM = 0.6):
+        self.Lambda = Lambda
+        self.minCIM = minCIM
+        self._reset_state()
 
 
+    def _reset_state(self):
+        self.numNodes = 0
+        self.weight = []
+        self.CountNode = []
+        self.adaptiveSig = []
